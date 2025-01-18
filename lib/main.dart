0a235b7bff122b758926ee5f51855c03de9f3765
felix-blueinno <felix@blueinnotechnology.com>
2025-01-18 12:20:30 +0800

    curtis l2 progress

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,36 @@ void main() {
 class MainApp extends StatelessWidget {
   MainApp({super.key});
 
+  // build == draw
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.greenAccent,
         drawer: Drawer(),
         appBar: AppBar(
           title: Text('App Bar Here'),
           backgroundColor: Colors.greenAccent,
           centerTitle: true,
         ),
-        body: SizedBox(
+        body: ListView.builder(
+          itemCount: 1000,
+          itemBuilder: (context, index) => Text('Child $index'),
+        ),
+      ),
+    );
+  }
+}
+
+class DummyRecipe extends StatelessWidget {
+  const DummyRecipe({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
           width: double.infinity,
           height: 80,
           child: Row(
@@ -35,7 +53,72 @@ class MainApp extends StatelessWidget {
             ],
           ),
         ),
-      ),
+
+        // ! Our recipe here:
+        SizedBox(
+          height: 300,
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Column(
+                  spacing: 16,
+                  children: [
+                    Text(
+                      'Strawberry Pavlova',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.pink.shade700,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text('My Recipe here'),
+
+                    // Rating:
+                    Row(
+                      children: [
+                        Icon(Icons.star),
+                        Icon(Icons.star),
+                        Icon(Icons.star),
+                        Icon(Icons.star),
+                        Icon(Icons.star),
+                        Spacer(),
+                        Text('170 Reviews'),
+                      ],
+                    ),
+
+                    Row(
+                      children: [
+                        Icon(Icons.book),
+                        Icon(Icons.timelapse),
+                        Icon(Icons.food_bank),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text('PREP:'),
+                        Text('COOK:'),
+                        Text('FEEDS:'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text('25 min'),
+                        Text('1 hr'),
+                        Text('4-6'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Placeholder(),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
