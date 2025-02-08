261caf8c6f7b5c7feef27110c6b02ec2bf863b73
felix-blueinno <felix@blueinnotechnology.com>
2025-02-08 09:14:11 +0800

    jayden l2 progress

--- lib/main.dart
+++ lib/main.dart
@@ -11,31 +11,104 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.greenAccent,
+        backgroundColor: Colors.white,
         drawer: Drawer(),
         appBar: AppBar(
-          title: Text('App Bar Here'),
-          backgroundColor: Colors.greenAccent,
+          title: Text('What Is A Pavlova'),
+          titleTextStyle: TextStyle(fontSize: 32),
           centerTitle: true,
+          backgroundColor: Color.fromARGB(0, 255, 255, 255),
         ),
-        body: SizedBox(
+        body: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return Text('Line $index');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class lesson1 extends StatelessWidget {
+  const lesson1({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
           width: double.infinity,
           height: 80,
           child: Row(
             children: [
-              Icon(Icons.person, size: 48),
+              Icon(Icons.person, size: 62),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Felix Wong'),
-                  Text('Student'),
-                ],
+                children: [Text('Jayden Tang'), Text('student')],
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Column(
+                  children: [
+                    Text('Pavlovas'),
+                    Text(
+                        'Pavlova is a meringue-based dessert yada yada yada...'),
+                    Row(
+                      children: [
+                        Icon(Icons.star),
+                        Icon(Icons.star),
+                        Icon(Icons.star),
+                        Icon(Icons.star),
+                        Icon(Icons.star),
+                        Spacer(),
+                        Text('170 reviews')
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Icon(Icons.kitchen),
+                        Icon(Icons.timelapse),
+                        Icon(Icons.food_bank)
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text('PREP:'),
+                        Text('COOK:'),
+                        Text('FEEDS:'),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text('25 mins:'),
+                        Text('1 hr:'),
+                        Text('4-6:'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Placeholder(),
               ),
             ],
           ),
         ),
-      ),
+      ],
     );
   }
 }
